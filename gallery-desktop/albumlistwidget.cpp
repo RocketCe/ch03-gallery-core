f1f56951a0f91375fd2d2d06cbf37d8153e53150
hfpgce <hfpgce@126.com>
2020-01-30 15:56:32 +0800

    add code in albumlistwidget.cpp

--- gallery-desktop/albumlistwidget.cpp
+++ gallery-desktop/albumlistwidget.cpp
@@ -1,14 +1,45 @@
 #include "albumlistwidget.h"
 #include "ui_albumlistwidget.h"
 
+#include<QInputDialog>
+#include"albummodel.h"
+
 AlbumListWidget::AlbumListWidget(QWidget *parent) :
     QWidget(parent),
-    ui(new Ui::AlbumListWidget)
+    ui(new Ui::AlbumListWidget),
+    mAlbumModel(nullptr)
 {
     ui->setupUi(this);
+    connect(ui->createAlbumButton,&QPushButton::clicked,this,&AlbumListWidget::createAlbum);
 }
 
 AlbumListWidget::~AlbumListWidget()
 {
     delete ui;
+}
+
+void AlbumListWidget::setModel(AlbumModel *model)
+{
+    mAlbumModel=model;
+    ui->albumList->setModel(model);
+}
+
+void AlbumListWidget::setSelectionModel(QItemSelectionModel *selectionModel)
+{
+    ui->albumList->setSelectionModel(selectionModel);
+}
+
+void AlbumListWidget::createAlbum()
+{
+    if(!mAlbumModel){
+        return;
+    }
+
+    bool ok;
+    QString albumName=QInputDialog::getText(this,"Create a new Album","Choose an name",QLineEdit::Normal,"New album",&ok);
+    if(ok && !albumName.isEmpty()){
+        Album album(albumName);
+        QModelIndex createIndex=mAlbumModel->addAlbum(album);
+        ui->albumList->setCurrentIndex(createIndex);
+    }
 }
